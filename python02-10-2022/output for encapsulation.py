2bfd5aa6d5ae343a7184fcd66ef9cb81ac511e8c
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-10-03 09:46:28 +0530

    Add files via upload

---
+++ python02-10-2022/output for encapsulation.py
@@ -0,0 +1,28 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: D:/Documents/python practice/python encapsulation.py ========
+updating software
+driving
+Traceback (most recent call last):
+  File "D:/Documents/python practice/python encapsulation.py", line 14, in <module>
+    blackcar.__updatesoftware()
+AttributeError: 'car' object has no attribute '__updatesoftware'. Did you mean: '_car__updatesoftware'?
+>>> 
+========= RESTART: D:/Documents/python practice/python encapsulation.py ========
+driving
+Traceback (most recent call last):
+  File "D:/Documents/python practice/python encapsulation.py", line 26, in <module>
+    blackcar.__updatesoftware()
+AttributeError: 'car' object has no attribute '__updatesoftware'. Did you mean: '_car__updatesoftware'?
+>>> 
+========= RESTART: D:/Documents/python practice/python encapsulation.py ========
+driving
+200
+150
+>>> 
+========= RESTART: D:/Documents/python practice/python encapsulation.py ========
+driving
+200
+driving
+200
