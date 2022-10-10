8fddeb2dd5d00147f9e38068fafe34f7ffa3a91c
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-10-10 10:21:22 +0530

    Add files via upload

---
+++ python06-10-2022/dictionary.py
@@ -0,0 +1,58 @@
+
+'''
+d = {}
+class A:
+    pass
+a = A()
+d.update({a:"i am object",A: "i am class"})
+print(d)
+print(id(d))
+print(id(a))
+print(id(A))
+
+
+d[0] = "hello"
+print(d)
+
+
+d ={}
+d[(1,2)] = "hi"
+print(d)
+print(id(d))
+d[[1,2]] = "hello"
+print(d)
+d[{1,2}] = "hey"
+print(d)
+'''
+
+d = {}
+class A:
+    pass
+a = A()
+d.update({a:"i am object",A: "i am class"})
+print(d)
+print(id(d))
+print(id(a))
+print(id(A))
+
+
+d{1} = "dict"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python06-10-2022/outpur for python functions.py
@@ -0,0 +1,59 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+result is: 11
+
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+the sum is : 30
+the sum is : 10
+
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+a:10    b:20
+the sum is : 30
+a:5    b:5
+the sum is : 10
+
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+a:10    b:20
+the sum is : 30
+a:5    b:5
+the sum is : 10
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+a:10    b:0
+the sum is : 10
+a:5    b:5
+the sum is : 10
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+Traceback (most recent call last):
+  File "D:/Documents/python practice/python functions.py", line 62, in <module>
+    add(10,20,40,30,20,66)
+  File "D:/Documents/python practice/python functions.py", line 58, in add
+    total = a+b
+NameError: name 'b' is not defined
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+the sum is : 10
+the sum is : 30
+the sum is : 70
+the sum is : 100
+the sum is : 120
+the sum is : 186
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+the sum is : 186
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+a:7    b:8
+the sum is : 15
+the sum is 10
+>>> 
+=========== RESTART: D:/Documents/python practice/python functions.py ==========
+1727551766896 1727551766896
+1727551766896 1727551766896
+1727551766960 1727551766992
+a:7    b:8
+the sum is : 15
+the sum is 10
